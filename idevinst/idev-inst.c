4d71ce309c3bb55094819cbbb8ae4548dbcb4f6b
Murray Lee <murray.lee@beckietech.com>
2012-07-10 17:31:05 +0800

    1st minimal libimobiledevice program

--- idevinst/idev-inst.c
+++ idevinst/idev-inst.c
@@ -1,2 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <libimobiledevice/libimobiledevice.h>
+
 int main()
-{}
+{
+	idevice_t	phone;
+	char		*uuid = "a0a1a3ee9708777fcd7f052b8c849f1d5d9b0fb6";
+
+	if (IDEVICE_E_SUCCESS != idevice_new(&phone, uuid)) {
+		puts("I found no iPhone.");
+		exit(1);
+	}
+
+	return 0;
+}
+
+
