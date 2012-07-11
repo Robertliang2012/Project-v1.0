3bf3afaec22435d35da167618f9f6fb1f8c8309d
anna.lee <anna.lee@beckietech.com>
2012-07-11 18:48:16 +0800

    Unmodified version

--- idevinst/idev-inst.c
+++ idevinst/idev-inst.c
@@ -10,10 +10,10 @@
 
 #include <libimobiledevice/libimobiledevice.h>
 #include <libimobiledevice/lockdown.h>
+#include <libimobiledevice/notification_proxy.h>
 
 
 int		notified = 0;
-
 
 static void notifier(const char *, void *);
 
@@ -23,17 +23,31 @@ int main()
     idevice_t			phone = NULL;
     char				*uuid = "a0a1a3ee9708777fcd7f052b8c849f1d5d9b0fb6";
     lockdownd_client_t	client = NULL;
+	np_client_t 		np = NULL;
     const char			*noties[3] = {NP_APP_INSTALLED, NP_APP_UNINSTALLED, NULL};
-
+	uint16_t 			port;
+	int 				notification_expected = 0;
+	
     idevice_new(&phone, uuid);
     lockdownd_client_new_with_handshake(phone, &client, "idev-inst");
     lockdownd_start_service(client, "com.apple.mobile.notification_proxy", &port);
     np_client_new(phone, port, &np);
-
+	
     np_set_notify_callback(np, notifier, NULL);
+	np_observe_notifications(np, noties);
+	
+	setbuf(stdout, NULL);
+	
+	notification_expected = 0;
 
+
+	
     return 0;
 }
+
+	
+	
+	
 
 
 static void notifier(const char *notification, void *unused)
