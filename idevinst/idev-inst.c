dfea9e59d5c21efc0aaebfd8bdbc76ff9e1e6afa
Murray Lee <murray.lee@beckietech.com>
2012-07-12 20:03:50 +0800

    uncrustifieeeeeeeeeeeeeeed

--- idevinst/idev-inst.c
+++ idevinst/idev-inst.c
@@ -12,39 +12,43 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define DIR_HOME	"/beckie"
-#define DIR_APPDATA	"/beckie/appdata"
+#define DIR_HOME    "/beckie"
+#define DIR_APPDATA "/beckie/appdata"
 
 int mkworkdir(void);
 
 int main()
 {
-	mkworkdir();
-	
-	
+    mkworkdir();
+
+
     return 0;
 }
 
+
 int mkworkdir()
 {
-	struct stat	st;
-	int	n;
+    struct stat st;
+    int			n;
 
-	if (chdir(DIR_HOME) < 0)	{
-		perror("chdir");
-		exit(1);
-	}
+    if (chdir(DIR_HOME) < 0) {
+        perror("chdir");
+        exit(1);
+    }
 
-	n = stat(DIR_APPDATA, &st);
-	if (n < 0)	{
-		if (errno != ENOENT)	{
-			perror("stat");
-			exit(1);
-		} else	{
-			mkdir(DIR_APPDATA, 0755);
-		}
-	} else if (st.st_mode != 0755)
-		chmod(DIR_APPDATA, 0755);
+    n = stat(DIR_APPDATA, &st);
+    if (n < 0) {
+        if (errno != ENOENT) {
+            perror("stat");
+            exit(1);
+        } else {
+            mkdir(DIR_APPDATA, 0755);
+        }
+    } else if (st.st_mode != 0755) {
+        chmod(DIR_APPDATA, 0755);
+    }
 
-	return n;
+    return n;
 }
+
+
