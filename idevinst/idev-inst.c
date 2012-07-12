15fd5a247d984a77ed35441844767efbe76dfc76
Murray Lee <murray.lee@beckietech.com>
2012-07-12 23:46:51 +0800

    some dissected libimobiledevice API

--- idevinst/idev-inst.c
+++ idevinst/idev-inst.c
@@ -1,54 +1,42 @@
 /*************************************************************************
-    This program should merge idevicenistaller.c
-      and ifuse.c to accomplish the automation backup process.
+    This program should merge idevicenistaller.c and ifuse.c
+	    to accomplish the automation backup process.
 
                         --Murray
 *************************************************************************/
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-
-#define DIR_HOME    "/beckie"
-#define DIR_APPDATA "/beckie/appdata"
-
-int mkworkdir(void);
+int startup_config(void);
 
 int main()
 {
-    mkworkdir();
+    startup_config();
 
+	/* 
+		Consequently, we need
+			idevice_new()							-> to get phone
+			lockdownd_client_new_with_handshake()	-> to get client
+			lockdownd_start_service()				-> to get port
+
+				client & port are both vital to communicate with iPhone
+
+			instproxy_client_new()					-> to get ipc
+		
+				ipc is the most important to command iPhone.
+
+		
+			np_client_new()				-> to get np (which stands for notification)
+			np_set_notify_callback()	-> to setup notification handler
+			np_observe_notifications	-> to capture notifications
+	 */
+	
 
     return 0;
 }
 
 
-int mkworkdir()
+int startup_config()
 {
-    struct stat st;
-    int			n;
-
-    if (chdir(DIR_HOME) < 0) {
-        perror("chdir");
-        exit(1);
-    }
-
-    n = stat(DIR_APPDATA, &st);
-    if (n < 0) {
-        if (errno != ENOENT) {
-            perror("stat");
-            exit(1);
-        } else {
-            mkdir(DIR_APPDATA, 0755);
-        }
-    } else if (st.st_mode != 0755) {
-        chmod(DIR_APPDATA, 0755);
-    }
-
-    return n;
+    return 0;
 }
 
 
