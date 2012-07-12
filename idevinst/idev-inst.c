832024be567be64b23ca699bfbae25107a6e45a4
Murray Lee <murray.lee@beckietech.com>
2012-07-12 23:50:44 +0800

    just forgot to commence 'make' before committing

--- idevinst/idev-inst.c
+++ idevinst/idev-inst.c
@@ -1,6 +1,6 @@
 /*************************************************************************
     This program should merge idevicenistaller.c and ifuse.c
-	    to accomplish the automation backup process.
+        to accomplish the automation backup process.
 
                         --Murray
 *************************************************************************/
@@ -11,24 +11,24 @@ int main()
 {
     startup_config();
 
-	/* 
-		Consequently, we need
-			idevice_new()							-> to get phone
-			lockdownd_client_new_with_handshake()	-> to get client
-			lockdownd_start_service()				-> to get port
+    /*
+        Consequently, we need
+            idevice_new()							-> to get phone
+            lockdownd_client_new_with_handshake()	-> to get client
+            lockdownd_start_service()				-> to get port
 
-				client & port are both vital to communicate with iPhone
+                client & port are both vital to communicate with iPhone
 
-			instproxy_client_new()					-> to get ipc
-		
-				ipc is the most important to command iPhone.
+            instproxy_client_new()					-> to get ipc
 
-		
-			np_client_new()				-> to get np (which stands for notification)
-			np_set_notify_callback()	-> to setup notification handler
-			np_observe_notifications	-> to capture notifications
-	 */
-	
+                ipc is the most important to command iPhone.
+
+
+            np_client_new()				-> to get np (which stands for notification)
+            np_set_notify_callback()	-> to setup notification handler
+            np_observe_notifications	-> to capture notifications
+     */
+
 
     return 0;
 }
